edfcdf4bd3ada04afdafba06d10c1d37b1a30826
Keeghan McGarry <keeghan_m@live.com>
2023-07-28 20:02:57 +0100

    Swapping challenges

--- src/app/Challenges.ts
+++ src/app/Challenges.ts
@@ -4,20 +4,19 @@ export const Challenges = [
         name: "Tutorial Challenge",
         description: "Welcome to the tutorial! All you need to do is correct the below invalid CSS to make the 'wrong' design look like the 'correct' design.",
         HTML: `<h1><span>Hello,</span> world.</h1>`,
-        generalCSS: `html,body {
-                margin: 0;
-                padding: 0;
-                height:100%;
-                overflow:hidden;
-            }
-
-            body {
-                background: #666;
-                color: white;
-                font-family: sans-serif;
-                display: grid;
-                place-items: center;
-            }`,
+        generalCSS: `* {
+  margin: 0;
+  padding: 0;
+}
+body {
+font-family: sans-serif;
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
+  background: #666;
+  overflow:hidden;
+}`,
         correctCSS: `h1 {
             color: rgb(100,100,255);
         }
@@ -28,5 +27,70 @@ export const Challenges = [
         wrongCSS: `h1 {
             color: rgb(100,100,255);
         }`,
-        }
+        },
+    {
+        id: 1,
+        name: "Challenge 1",
+        description: "This is the first challenge! All you need to do is correct the below invalid CSS to make the 'wrong' design look like the 'correct' design.",
+        HTML: `
+        <div>
+        <input type="text" placeholder="Enter your name here" />
+        <button>Submit</button>
+        </div>
+        `,
+        generalCSS: `* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: sans-serif;
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100vw;
+  background: #666;
+  overflow:hidden;
+}
+            `,
+        correctCSS: `div {
+  display: flex;
+  gap: 0;
+}
+input {
+  font-size: 1.5rem;
+  border: none;
+  padding: 1rem 2rem;
+  background: rgb(210,180,250);
+  border-radius: 15px 0 0 15px;
+  color: black;
+}
+button {
+  border: none;
+  background: rgb(80,30,155);
+  color: white;
+  font-size: 1.5rem;
+  padding: 1rem 2rem;
+  border-radius: 0 15px 15px 0;
+}`,
+        wrongCSS: `div {
+  display: flex;
+  gap: 0;
+}
+input {
+  font-size: 1.5rem;
+  border: none;
+  padding: 1rem;
+  background: rgb(210,180,250);
+  border-radius: 15px 0 0 15px;
+  color: black;
+}
+button {
+  border: none;
+  background: rgb(80,30,155);
+  color: white;
+  font-size: 1.5rem;
+  padding: 1rem;
+  border-radius: 0 10px 10px 0;
+}`,
+        },
 ]
