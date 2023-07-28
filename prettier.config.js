24717b5f38253a57fcd23d593f5cce77e3357ddb
Keeghan McGarry <keeghan_m@live.com>
2023-07-28 18:31:21 +0100

    Initial Test

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Options} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss','prettier-plugin-astro'],
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
---
+++ src/app/_store.ts
@@ -0,0 +1,33 @@
+import { createSignal } from "solid-js";
+
+export const [challenge,setChallenge] = createSignal<Challenge>({
+    id: 0,
+    name: "Tutorial Challenge",
+    description: "Welcome to the tutorial! All you need to do is correct the below invalid CSS to make the 'wrong' design look like the 'correct' design.",
+    HTML: `<h1><span>Hello,</span> world.</h1>`,
+    generalCSS: `html,body {
+        margin: 0;
+        padding: 0;
+        height:100%;
+        overflow:hidden;
+    }
+
+    body {
+        background: #666;
+        color: white;
+        font-family: sans-serif;
+        display: grid;
+        place-items: center;
+    }`,
+    correctCSS: `h1 {
+    color: rgb(100,100,255);
+  }
+
+  h1 span {
+    color: rgb(80,30,155);
+  }`,
+    wrongCSS: `h1 {
+    color: rgb(100,100,255);
+  }`,
+});
+
