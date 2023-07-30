8becb22b6e60d30e4a1548ab4d7a77821dacb905
Keeghan McGarry <keeghan_m@live.com>
2023-07-30 07:50:02 +0100

    Better list

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -5,6 +5,7 @@ import Correct from './displays/Correct'
 import Wrong from './displays/Wrong'
 import Compare from './Compare'
 import { Swapper } from './Swapper'
+import ThemeToggle from './ThemeToggle'
 
 export default function Main() {
   onMount(() => {
@@ -13,24 +14,29 @@ export default function Main() {
   })
 
   return (
-    <div class='grid h-[80vh] grid-cols-[minmax(30vw,_450px)_1fr] py-12'>
-      <div class='flex flex-1 flex-col'>
-        <Editors />
+    <>
+      <div class='fixed z-50 flex w-full justify-end p-2 md:p-4 lg:p-6'>
+        <ThemeToggle />
       </div>
-      <div class='flex flex-col gap-2'>
-        <div class='w-full text-center'>
-          <h1 class='mb-2 mt-6 text-4xl text-blue-500'>{challenge().name}</h1>
-          <p class='mx-auto mb-6 max-w-screen-md text-xl leading-7'>
-            {challenge().description}
-          </p>
+      <div class='grid h-[80vh] grid-cols-[minmax(30vw,_450px)_1fr] py-12'>
+        <div class='flex flex-1 flex-col'>
+          <Editors />
         </div>
-        <div class='flex flex-1 gap-2'>
-          <Correct />
-          <Wrong />
+        <div class='flex flex-col gap-2'>
+          <div class='w-full text-center'>
+            <h1 class='mb-2 mt-6 text-4xl text-blue-500'>{challenge().name}</h1>
+            <p class='mx-auto mb-6 max-w-screen-md text-xl leading-7'>
+              {challenge().description}
+            </p>
+          </div>
+          <div class='flex flex-1 gap-2'>
+            <Correct />
+            <Wrong />
+          </div>
+          <Compare />
+          <Swapper />
         </div>
-        <Compare />
-        <Swapper />
       </div>
-    </div>
+    </>
   )
 }
